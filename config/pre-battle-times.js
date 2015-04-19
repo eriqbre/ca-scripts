18a2fa0416f05963e3203455206df716abd9e78e
ebreland <dw27cfh3>
2015-04-19 11:54:35 -0400

    - updating battle times for local timezone

--- config/pre-battle-times.js
+++ config/pre-battle-times.js
@@ -2,12 +2,12 @@
  * Created by ebreland on 4/12/15.
  */
 module.exports = [
-    {time: '45 9 * * 1-3', type: '100v100 morning weekdays'},
-    {time: '45 3 * * 0,4-6', type: '100v100 late weekends'},
-    {time: '45 15 * * *', type: '100v100 afternoon every day'},
-    {time: '45 21 * * *', type: '100v100'},
-    {time: '45 11 * * *', type: '10v10 morning every day'},
-    {time: '45 17 * * *', type: '10v10 afternoon every day'},
-    {time: '45 23 * * *', type: '10v10 evening every day'}
+    {time: '45 5 * * 1-3', type: '100v100 morning weekdays'},
+    {time: '45 23 * * 0,4-6', type: '100v100 late weekends'},
+    {time: '45 11 * * *', type: '100v100 afternoon every day'},
+    {time: '45 17 * * *', type: '100v100'},
+    {time: '45 8 * * *', type: '10v10 morning every day'},
+    {time: '45 14 * * *', type: '10v10 afternoon every day'},
+    {time: '45 20 * * *', type: '10v10 evening every day'}
 ];
 
