34eed37dcc7c3515e393d87d03323c33dfb02c97
ebreland <dw27cfh3>
2015-04-25 13:42:02 -0400

    - adding all workers to a single worker process

--- Procfile
+++ Procfile
@@ -1,4 +1,2 @@
 web: node server.js
-worker: node workers/loadouts.js
-#worker: node workers/lom-actions.js
-worker: node workers/maintenance.js
+worker: node workers.js
--- workers.js
+++ workers.js
@@ -1,22 +1,34 @@
 /**
  * Created by ebreland on 4/25/15.
- * run this job continuously throughout the day
  */
 
 var app = require('../app'),
     cronJob = require('cron').CronJob,
     cronfigs = require('../config/cron'),
+    changeLoadouts = require('../services/change-loadouts'),
     maintenance = require('../services/maintenance');
 
-app.listen(3025, function () {
-    console.log('starting maintenance cron');
+app.listen(3001, function () {
+    console.log('cron jobs');
 
+    // cron for changing loadouts
+    cronfigs.loadouts.times.map(function (cronfig) {
+        new cronJob({
+            cronTime: cronfig.time,
+            onTick: function () {
+                changeLoadouts(cronfig.type);
+            },
+            start: true,
+            timeZone: cronfigs.timeZone
+        });
+    });
+
+    // cron for basic maintenance tasks
     new cronJob({
         cronTime: cronfigs.maintenance.time,
         onTick: function () {
             maintenance(function (error, data) {
                 console.log('completed maintenance run');
-                console.log(data);
             });
         },
         start: true,
