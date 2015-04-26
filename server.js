18af5e34988b343e20255e48b0670f32743ede61
Eriq Breland <eriqbre@gmail.com>
2015-04-25 20:22:49 -0400

    - adding clustering

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var app = require('./app'),
+    cluster = require('cluster'),
+    cpus = require('os').cpus().length,
     port = process.env.PORT || 3010;
 
-app.listen(port);
-console.log('server running on port ' + port);
-
+if (cluster.isMaster){
+    for (var i = 0; i< cpus; i++){
+        cluster.fork();
+    }
+} else {
+    app.listen(port);
+    console.log('server running on port ' + port);
+}
--- workers.js
+++ workers.js
@@ -3,35 +3,43 @@
  */
 
 var app = require('./app'),
+    cluster = require('cluster'),
+    cpus = require('os').cpus().length,
     cronJob = require('cron').CronJob,
     cronfigs = require('./config/cron'),
     changeLoadouts = require('./services/change-loadouts'),
     maintenance = require('./services/maintenance');
 
-app.listen(cronfigs.port, function () {
-    console.log('cron jobs');
+if (cluster.isMaster){
+    for (var i = 0; i< cpus; i++){
+        cluster.fork();
+    }
+} else {
+    app.listen(cronfigs.port, function () {
+        console.log('cron jobs');
 
-    // cron for changing loadouts
-    cronfigs.loadouts.times.map(function (cronfig) {
+        // cron for changing loadouts
+        cronfigs.loadouts.times.map(function (cronfig) {
+            new cronJob({
+                cronTime: cronfig.time,
+                onTick: function () {
+                    changeLoadouts(cronfig.type);
+                },
+                start: true,
+                timeZone: cronfigs.timeZone
+            });
+        });
+
+        // cron for basic maintenance tasks
         new cronJob({
-            cronTime: cronfig.time,
+            cronTime: cronfigs.maintenance.time,
             onTick: function () {
-                changeLoadouts(cronfig.type);
+                maintenance(function (error, data) {
+                    console.log('completed maintenance run');
+                });
             },
             start: true,
             timeZone: cronfigs.timeZone
         });
     });
-
-    // cron for basic maintenance tasks
-    new cronJob({
-        cronTime: cronfigs.maintenance.time,
-        onTick: function () {
-            maintenance(function (error, data) {
-                console.log('completed maintenance run');
-            });
-        },
-        start: true,
-        timeZone: cronfigs.timeZone
-    });
-});
+}
