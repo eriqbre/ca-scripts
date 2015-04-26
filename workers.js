c391efddbd02d70401900d75556384d925249066
ebreland <dw27cfh3>
2015-04-25 23:09:47 -0400

    - removing cluster for worker process

--- workers.js
+++ workers.js
@@ -10,40 +10,30 @@ var app = require('./app'),
     changeLoadouts = require('./services/change-loadouts'),
     maintenance = require('./services/maintenance');
 
-if (cluster.isMaster){
-    for (var i = 0; i< cpus; i++){
-        cluster.fork();
-    }
+app.listen(cronfigs.port, function () {
+    console.log('cron jobs');
 
-    cluster.on('exit', function (worker, code, signal) {
-        cluster.fork();
-    });
-} else {
-    app.listen(cronfigs.port, function () {
-        console.log('cron jobs');
-
-        // cron for changing loadouts
-        cronfigs.loadouts.times.map(function (cronfig) {
-            new cronJob({
-                cronTime: cronfig.time,
-                onTick: function () {
-                    changeLoadouts(cronfig.type);
-                },
-                start: true,
-                timeZone: cronfigs.timeZone
-            });
-        });
-
-        // cron for basic maintenance tasks
+    // cron for changing loadouts
+    cronfigs.loadouts.times.map(function (cronfig) {
         new cronJob({
-            cronTime: cronfigs.maintenance.time,
+            cronTime: cronfig.time,
             onTick: function () {
-                maintenance(function (error, data) {
-                    console.log('completed maintenance run');
-                });
+                changeLoadouts(cronfig.type);
             },
             start: true,
             timeZone: cronfigs.timeZone
         });
     });
-}
+
+    // cron for basic maintenance tasks
+    new cronJob({
+        cronTime: cronfigs.maintenance.time,
+        onTick: function () {
+            maintenance(function (error, data) {
+                console.log('completed maintenance run');
+            });
+        },
+        start: true,
+        timeZone: cronfigs.timeZone
+    });
+});
