ab02a49a8e52db1186f2502017a41d144db2c32a
ebreland <ebreland@madmobile.com>
2015-04-29 15:45:25 -0400

    - adding clustering - adding mongo session store - adding server restarting for server.js

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 var app = require('./app'),
 	cluster = require('express-cluster'),
 	cpus = require('os').cpus().length,
-    port = process.env.PORT || 3010;
+	port = process.env.PORT || 3010,
+	environment = process.env.NODE_ENV || 'development';
 
-// for production
-cluster(function(worker) {
+if (process.env.NODE_ENV === 'production') {
+	// for production, no debugging possible
+	cluster(function (worker) {
+		app.listen(port);
+		console.log('server running on port ' + port);
+		console.log('cluster environment for ' + environment);
+	}, {
+		count: cpus,
+		respawn: true,
+		verbose: true
+	});
+} else {
+	// use when debugging
 	app.listen(port);
 	console.log('server running on port ' + port);
-}, {count: cpus});
+	console.log('single process environment for ' + environment);
+}
 
-// uncomment when debugging
-// app.listen(port);
