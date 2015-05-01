f5eb2b663a47c7606bd06984b0f4bc08b23c2149
ebreland <ebreland@madmobile.com>
2015-05-01 15:03:28 -0400

    - adding overrides to lom-actions

--- config/lom-actions.js
+++ config/lom-actions.js
@@ -2,9 +2,11 @@
  * Created by ebreland on 4/24/2015.
  */
 
-module.exports = {
-	healthPerActionTarget: 3000,
-	ceiling: 950,
-	floor: 100,
-	healthPercentage: 99
+module.exports = function (options) {
+	return {
+		healthPerActionTarget: options.healthPerActionTarget || 3000,
+		ceiling: options.ceiling || 950,
+		floor: options.floor || 100,
+		healthPercentage: options.healthPercentage || 93
+	}
 };
