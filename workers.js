ef71e00532f1061dc29951466d17cadb4f278ce4
Eriq Breland <eriqbre@gmail.com>
2015-05-12 20:34:48 -0400

    - adding process exit handler to worker

--- workers.js
+++ workers.js
@@ -21,7 +21,8 @@ app.listen(cronfigs.port, function () {
             cronTime: cronfig.time,
             onTick: function () {
                 changeLoadouts({id: cronfig.type}, function (data) {
-                    console.log(cronfig.type + ' loadouts worker finished')
+                    console.log(cronfig.type + ' loadouts worker finished');
+                    process.on('exit', exitHandler.bind(null, { exit: true }));
                 });
             },
             start: true,
@@ -35,6 +36,7 @@ app.listen(cronfigs.port, function () {
         onTick: function () {
             maintenance(function (error, data) {
                 console.log('completed maintenance run');
+                process.on('exit', exitHandler.bind(null, { exit: true }));
             });
         },
         start: true,
@@ -47,6 +49,7 @@ app.listen(cronfigs.port, function () {
         onTick: function () {
             lom(function (error, data) {
                 console.log('completed lom-check');
+                process.on('exit', exitHandler.bind(null, { exit: true }));
             });
         },
         start: true,
@@ -59,6 +62,7 @@ app.listen(cronfigs.port, function () {
 		onTick: function () {
 			battleActions({role: '10v10-actions'}, function (error, data) {
 				console.log('completed battle-actions for 10v10');
+                process.on('exit', exitHandler.bind(null, { exit: true }));
 			});
 		},
 		start: true,
@@ -71,6 +75,7 @@ app.listen(cronfigs.port, function () {
 		onTick: function () {
 			battleCollection({role: 'auto-collect-10v10'}, function (error, data) {
 				console.log('completed battle-collection for 10v10');
+                process.on('exit', exitHandler.bind(null, { exit: true }));
 			});
 		},
 		start: true,
@@ -84,6 +89,7 @@ app.listen(cronfigs.port, function () {
 			onTick: function () {
 				battleActions({role: '100v100-actions'}, function (error, data) {
 					console.log('completed battle-actions for 100v100');
+                    process.on('exit', exitHandler.bind(null, { exit: true }));
 				});
 			},
 			start: true,
@@ -98,10 +104,16 @@ app.listen(cronfigs.port, function () {
 			onTick: function () {
 				battleCollection({role: 'auto-collect-100v100'}, function (error, data) {
 					console.log('completed battle-collection for 100v100');
+                    process.on('exit', exitHandler.bind(null, { exit: true }));
 				});
 			},
 			start: true,
 			timeZone: cronfigs.timeZone
 		});
 	});
+
+    // handle process exits
+    function exitHandler(options, error){
+        process.exit();
+    }
 });
